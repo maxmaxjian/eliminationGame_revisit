dc78bf7618b2fcd3d514b11e9fb58b7130b16cd5
Jian Wei <jianwei@jianwei-deb8-64>
2017-01-18 11:36:15 -0500

    second version

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 
 class solution {
     lastRemaining(n) {
-	var vec = []
-	for (var i = 1; i <= n; i++)
-	    vec.push(i);
 	var lastArr = new Array();
 	var i = 0
 	lastArr.push(n)
 	while (lastArr[lastArr.length-1]-this.power(2,i) > 0) {
 	    var last
-	    if (i == 0) {
-		last = lastArr[lastArr.length-1]%2 == 0?lastArr[lastArr.length-1]:lastArr[lastArr.length-1]-1
-	    }
+	    if ((Math.ceil(lastArr[lastArr.length-1]/this.power(2,i)-1)+1)%2 != 0)
+		last = lastArr[lastArr.length-1]-this.power(2,i)
 	    else {
-		if (lastArr[lastArr.length-1]/this.power(2,i)%2 != 0)
+		if (i%2 != 0)
 		    last = lastArr[lastArr.length-1]-this.power(2,i)
-		else {
-		    if (i%2 != 0)
-			last = lastArr[lastArr.length-1]-this.power(2,i)
-		    else
-			last = lastArr[lastArr.length-1]
-		}
+		else
+		    last = lastArr[lastArr.length-1]
 	    }
 	    lastArr.push(last);
 	    i++;
@@ -36,8 +28,47 @@ class solution {
 	    return base*this.power(base,n-1)
 	    
     }
+
+    brute_force(n) {        
+	var vec = []
+	for (var i = 1; i <= n; i++)
+	    vec.push(i);
+        if (vec.length < 20)
+            console.log(vec)
+        var i = 0;        
+        while (vec.length > 1) {
+            var next = []
+            if (i%2 == 0) {
+                for (var j = 0; j < vec.length; j++)
+                    if (j%2 != 0)
+                        next.push(vec[j])
+            }
+            else {
+                if (vec.length%2 == 0) {
+                    for (var j = 0; j < vec.length; j++) {
+                        if (j%2 == 0)
+                            next.push(vec[j])
+                    }
+                }
+                else {
+                    for (var j = 0; j < vec.length; j++)
+                        if (j%2 != 0)
+                            next.push(vec[j])
+                }
+            }
+            if (next.length < 20)
+                console.log(next)
+            vec = next
+            i++;
+        }
+        return vec[0]
+    }
 }
 
-var n = 13
+var n = 190
 var soln = new solution()
 var last = soln.lastRemaining(n)
+console.log("The last remaining number is: "+last)
+var ans_last = soln.brute_force(n)
+if (last == ans_last)
+    console.log("True")
